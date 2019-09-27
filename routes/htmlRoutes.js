5dacc73efd1c505d14593216fbb9478a21486fa8
Tim Sherry <tsherry82@gmail.com>
2019-09-27 16:46:41 -0400

    added home page and logic for hangman

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -2,26 +2,31 @@ var db = require("../models");
 var express = require("express");
 var path = require("path");
 
+var app = express();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
  
 
 module.exports = function(app) {
+  app.get("/",function(req,res){
+    res.sendFile(path.resolve(__dirname,"../views/index.html"))
+  });
+
   app.get("/chipGame",function(req,res){
-    res.sendFile(path.join(__dirname,"chipGuessGame.html"))
+    res.sendFile(path.resolve(__dirname,"../views/games/chipGuessGame.html"))
   });
   
   app.get("/wordGame",function(req,res){
-    res.sendFile(path.join(__dirname,"wordGuessGame.html"))
+    res.sendFile(path.resolve(__dirname,"../views/games/wordGuessGame.html"))
   });
   
   app.get("/slotsGame",function(req,res){
-    res.sendFile(path.join(__dirname,"slotsGame.html"))
+    res.sendFile(path.resolve(__dirname,"../views/games/slotsGame.html"))
   });
   
   app.get("/blackJackGame",function(req,res){
-    res.sendFile(path.join(__dirname,"blackJackGame.html"))
+    res.sendFile(path.resolve(__dirname,"../views/games/blackJackGame.html"))
   });
 };
 
