5cf23ba8d35d607369e79462e6fd6e999b0eedff
AdamM <ajmrozinski@gmail.com>
2019-10-02 01:49:02 -0400

    added basic logout button, clears session storage and redirects to home page

--- public/js/index.js
+++ public/js/index.js
@@ -10,6 +10,12 @@ $("#user-name").text(name)
 
 $("#user-chips").text(coins)
 
+// logout button 
+$("#logoutBtn").on("click",()=>{
+  sessionStorage.clear()
+  document.location.replace("/")
+})
+
 // The API object contains methods for each kind of request we'll make
 var API = {
   saveExample: function(example) {
@@ -103,3 +109,4 @@ var handleDeleteBtnClick = function() {
 // Add event listeners to the submit and delete buttons
 $submitBtn.on("click", handleFormSubmit);
 $exampleList.on("click", ".delete", handleDeleteBtnClick);
+
